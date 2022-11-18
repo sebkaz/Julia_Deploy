c52eb92987704b3e976200d49f35400b96258fc3
sebkaz <seba@sebastianzajac.pl>
2022-11-18 18:58:45 +0100

    fix dataframe types

--- app.jl
+++ app.jl
@@ -1,6 +1,6 @@
 using Genie, Genie.Renderer.Json, Genie.Requests
 using HTTP, GLM, DataFrames, LinearAlgebra
-using BSON
+using BSON, JSON3
 
 BSON.@load "model.bson" model
 
@@ -10,27 +10,38 @@ end
 
 route("/api_one", method = GET) do
   x_test = params(:data, 0)
-  if typeof(x_test) == String
+   
+  # if occursin("[", x_test)
+    # tutaj dopisac kod parsujący "[1,2,3]" na [1,2,3]
+  # else
     try
       x_test = parse(Int64, x_test)
     catch e
       return "You should have entered a numeric value for data"
     end
-  end
   pred = predict(model, DataFrame(size = x_test))
   (:data => ([x_test]), :prediction => (pred)) |> json
-end
+  end
 
 route("/api/predict", method = POST) do
-    message = jsonpayload()
-    size_values = message["data"]
-    pred  = predict(model, DataFrame(size = size_values))
-  (:data => (value["data"]), :prediction => (pred)) |> json
+    values = jsonpayload()
+    try 
+    pred  = predict(model, DataFrame(size = values["data"]))
+    pred2 = predict(model, DataFrame(size = values["data2"]))
+  (:status => "ok", 
+  :data => (values["data"]), 
+  :prediction => (pred),
+  :prediction2 => (pred2)) |> json
+    catch e
+  (:status => "ERROR", :data => (values["data"])) |> json
+    end
+
 end
 
 route("/example") do
-  response = HTTP.request("POST", "http://localhost:8000/api/predict", 
-  [("Content-Type", "application/json")], """{status:"ok","data":[1250,1532,2432]}""")
+  response = HTTP.post("http://localhost:8000/api/predict", 
+                      [("Content-Type", "application/json")],
+                      JSON3.write(Dict("data2"=> 1250, "data" => [1250,2341,15])))
   response.body |> String |> json
 end
 
--- request.jl
+++ request.jl
@@ -1,13 +1,9 @@
 using HTTP
 using JSON3
 
-req = HTTP.request("POST", "http://localhost:8000/api/predict", 
- [("Content-Type", "application/json")], """{"data":[1250,1532,2432]}""")
-
-
-# req = HTTP.post("http://127.0.0.1:8000",
-#                 ["Content-Type" => "application/json"],
-#                 JSON3.write((status="ok", data=[1250,2341,15]) )
-#                 )
+req = HTTP.post("http://127.0.0.1:8000/api/predict",
+                ["Content-Type" => "application/json"],
+                JSON3.write(Dict("data" => [1250,2341,15]))
+                )
 
 print(String(req.body))
